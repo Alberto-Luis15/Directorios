ec913c59fd45c221807e8bb74330be0c7ec4549a
Alberto-Luis15 <Luis_arq8@outlook.es>
2020-04-21 13:40:38 -0500

    programa de directorios

---
+++ src/demostracionfile/crear.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demostracionfile;
+import java.io.FileNotFoundException;
+import java.lang.SecurityException;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Escuela
+ */
+public class crear {
+    
+}
